ceaefa1c50158a0b8680f6079f093abada3673d9
HystericalDragon <HystericalDragons@proton.me>
2024-04-20 22:35:20 +0800

    Improve example
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- example/cac/cac.go
+++ example/cac/cac.go
@@ -9,13 +9,15 @@ import (
 	"github.com/txthinkinginc/cac"
 )
 
-var flagSet *flag.FlagSet
-
 func init() {
 	if len(os.Args) == 2 {
 		switch os.Args[1] {
 		case "-h", "--help", "help":
-			goto stdParse
+			defer func() {
+				flag.Usage()
+				os.Exit(0)
+			}()
+			goto stdflag
 		}
 
 		fileContent, err := os.ReadFile(os.Args[1])
@@ -24,18 +26,14 @@ func init() {
 			return
 		}
 
-		flagSet = flag.NewFlagSet("custom flag set", flag.ExitOnError)
-		flagSet.Usage = func() {
-			fmt.Println("Help of custom flag set...")
-		}
-		_ = flagSet.Parse(cac.Parse(string(fileContent)))
+		flag.CommandLine.Parse(cac.Parse(string(fileContent)))
 		return
 	}
 
-stdParse:
+stdflag:
 	flag.Parse()
 }
 
 func main() {
-	fmt.Println(flagSet.Args())
+	fmt.Println(flag.Args())
 }
